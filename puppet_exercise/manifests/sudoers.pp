cb40b9920ca2a1fe575d4270fa8b8e8d12a233f9
unknown <omaraakamal@gmail.com>
2021-02-22 14:18:58 -0600

    push 2.93

--- puppet_exercise/manifests/sudoers.pp
+++ puppet_exercise/manifests/sudoers.pp
@@ -1,7 +1,7 @@
 # Class: puppet_exercise::sudoers:
 #
 #
-class puppet_exercise::sudoers: {
+class puppet_exercise::sudoers {
 	require puppet_exercise::users
 	require puppet_exercise::groups
 	$sudoinfo = hiera('puppet_exercise::sudoers', {})
